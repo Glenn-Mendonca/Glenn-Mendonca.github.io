348802204c8e23af3f6a424bd3ee38ab17af23b5
Glenn Mendonca <glenem17@gmail.com>
2023-07-10 18:49:05 +0530

    feat: Competitive component Added

--- src/components/Competitive.jsx
+++ src/components/Competitive.jsx
@@ -1,10 +1,33 @@
-import {useState, useEffect} from 'react'
-import ReactApexChart from 'react-apexcharts'
+import { useState, useEffect } from "react";
+import ReactApexChart from "react-apexcharts";
 
 function Competitive() {
+  const [site, setSite] = useState("codeforces");
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // Fetch
+    console.log(site);
+  }, [site]);
   return (
-    <div>Competitive</div>
-  )
+    <div className="dark:bg-dark-secondary bg-light-secondary max-w-[500px] mx-auto rounded-md p-4 m-2">
+      <div className="flex justify-between">
+        <h1 className="text-xl my-auto dark:text-light-secondary text-dark-secondary">
+          Competitive Profiles
+        </h1>
+        <select
+          name="website"
+          className="bg-[#444448] text-white text-sm  rounded-md w-32 p-2 border-none"
+          onChange={(e) => setSite(e.target.value)}
+        >
+          <option value="leetcode">Leetcode</option>
+          <option value="codeforces">Codeforces</option>
+          <option value="codechef">Codechef</option>
+        </select>
+      </div>
+
+      <div className="h-64 w-full animate-pulse bg-light-secondary rounded-md mt-3"></div>
+    </div>
+  );
 }
 
-export default Competitive
+export default Competitive;
