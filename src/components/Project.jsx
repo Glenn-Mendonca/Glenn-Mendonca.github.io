9991f541ffff3a207eb40a65099b0581265ae66d
Glenn Mendonca <glenem17@gmail.com>
2023-07-10 19:01:43 +0530

    fix: Work & Project component func added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { BsFolder } from "react-icons/bs";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+import Icon from "./Icon";
 
-function Project() {
+function Project(props) {
   return (
-    <div>Project</div>
-  )
+    <div className="min-w-[320px] max-w-[450px] w-[48%] md:max-w-[400px] p-10 bg-neutral-800 hover:scale-105 transition-all duration-200 rounded-sm">
+      <div className="text-neutral-400 mb-5 flex  justify-between">
+        <div>
+          <BsFolder size={50} />
+        </div>
+        <div className="flex gap-2.5 mt-1">
+          {props.githubLink && (
+            <button
+              className="hover:text-neutral-100"
+              onClick={() => window.open(props.githubLink, "_blank")}
+            >
+              <FiGithub size={25} />
+            </button>
+          )}
+          {props.externalLink && (
+            <button
+              className="hover:text-neutral-100"
+              onClick={() => window.open(props.externalLink, "_blank")}
+            >
+              <FiExternalLink size={25} />
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="text-white font-medium text-xl">{props.projectName}</div>
+      <div className="text-neutral-400 text-sm">
+        {props.startDate} - {props.endDate}
+      </div>
+      <div className="mt-6 text-base mb-3 text-neutral-100 ">
+        {props.description}
+      </div>
+      <div className="gap-2 flex float-right">
+        {props.techStack.map((tech) => Icon({ icon: tech }))}
+      </div>
+    </div>
+  );
 }
 
-export default Project
+export default Project;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Icon from "./Icon";
 
-function Work() {
+function Work(props) {
   return (
-    <div>Work</div>
-  )
+    <div class="max-w-[90%] p-10 bg-neutral-800 lg:hover:scale-105 transition-all duration-200 rounded-sm mx-auto cursor-pointer my-3 ">
+      <div class="text-neutral-400 text-sm text-right">
+        {props.startDate} - {props.endDate}
+      </div>
+      <div class="text-white text-xl font-medium">{props.role}</div>
+      <div class="text-white text-lg">@{props.company}</div>
+      <div class="text-neutral-400 text-sm">{props.location}</div>
+      <div class="mt-6 text-base mb-3 text-neutral-100 w-2/3 text-">
+        {props.description}
+      </div>
+      <div class="gap-2 flex float-right">
+        {props.techStack.map((tech) => Icon({ icon: tech }))}
+      </div>
+    </div>
+  );
 }
 
-export default Work
+export default Work;
