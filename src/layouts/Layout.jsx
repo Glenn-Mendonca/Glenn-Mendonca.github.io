f279e6142b133a6741834bf62cfeccb07d123cc0
Glenn Mendonca <glenem17@gmail.com>
2023-07-10 18:48:40 +0530

    feat: Tobe done in future

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Layout() {
   return (
     <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
