f68041069f24edd3e3dbbe64be482f330d0741a3
Glenn Mendonca <glenem17@gmail.com>
2023-07-10 18:47:57 +0530

    feat: Added About comp with boilerplate

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h2 className="font-bold text-5xl text-dark-secondary dark:text-light-secondary">Glenn Mendonca</h2>
+      <h4 className="font-bold text-2xl py-5 text-dark-secondary dark:text-light-secondary">Full Stack Developer</h4>
+      <p className="dark:text-[#949495] leading-relaxed py-2 tracking-wider">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nemo id
+        maiores, amet quidem accusamus dolorem minus quisquam deserunt
+        recusandae similique eaque itaque.
+      </p>
+      <p className="dark:text-[#949495] leading-relaxed pb-2 tracking-wider">
+      Ipsam id, libero magni nemo ad iste
+        veritatis praesentium, tempora possimus facilis voluptates, doloremque
+        aliquid accusamus nesciunt obcaecati?
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
